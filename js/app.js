b8aa7025c7281751995f1da60c5ced60b513017c
Eric <coralvanda@gmail.com>
2016-07-13 10:46:26 +0200

    Finish app

--- js/app.js
+++ js/app.js
@@ -1,13 +1,63 @@
+var initialCats = [
+	{
+		clickCount: 0,
+		name: "Tabby",
+		imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+		imgAttribution: 'https://www.flickr.com/photos/bigtallguy/434164568',
+		nicknames: ["Tabs", "Tabsy", "Tabaroonie"]
+	},
+	{
+		clickCount: 0,
+		name: "Tiger",
+		imgSrc: "img/4154543904_6e2428c421_z.jpg",
+		imgAttribution: 'whatever',
+		nicknames: ['Tigger']
+	},
+	{
+		clickCount: 0,
+		name: "Scaredy",
+		imgSrc: 'img/22252709_010df3379e_z.jpg',
+		imgAttribution: 'blah',
+		nicknames: ['Casper']
+	},
+	{
+		clickCount: 0,
+		name: "Shadow",
+		imgSrc: 'img/1413379559_412a540d29_z.jpg',
+		imgAttribution: 'yup',
+		nicknames: ['Shooby']
+	}
+]
+
+
 var ViewModel = function() {
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-	this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
+	var self = this;
+
+	this.catList = ko.observableArray([]);
+
+	initialCats.forEach(function(catItem) {
+		self.catList.push( new Cat(catItem) );
+	});
+
+	this.currentCat = ko.observable( this.catList()[0] );
 
 	this.incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1);
+		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
 	};
 
+	this.changeCat = function(cat) {
+		self.currentCat(cat);
+	};
+
+};
+
+var Cat = function(data) {
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.imgAttribution = ko.observable(data.imgAttribution);
+	this.nicknames = ko.observableArray(data.nicknames);
+	
 	this.level = ko.computed(function() {
 		if (this.clickCount() < 9) {
 			return "Newborn";
@@ -18,10 +68,13 @@ var ViewModel = function() {
 		else if (this.clickCount() < 40) {
 			return "Teen";
 		}
-		else {
+		else if (this.clickCount() < 100) {
 			return "Adult";
 		}
+		else {
+			return "Ninja";
+		}
 	}, this);
-}
+};
 
 ko.applyBindings(new ViewModel())
